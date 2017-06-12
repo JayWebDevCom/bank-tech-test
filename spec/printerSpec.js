5a7a1ddbfa9ef2d7a01ece68f6732485c46e34b6
JayWebDevCom <jaiye.s@gmail.com>
2017-06-12 16:21:39 +0100

    add red printer test

--- spec/printerSpec.js
+++ spec/printerSpec.js
@@ -1,1 +1,11 @@
-console.log('printer')
+require('../lib/printer');
+var printer
+
+describe('Printer', function(){
+  beforeEach(function(){
+    printer = new Printer
+  })
+  it('can be instantiated', function(){
+    expect(printer instanceof Printer).toBeTruthy();
+  })
+})
