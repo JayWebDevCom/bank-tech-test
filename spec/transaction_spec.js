6da8f09d9cdb0975f0ff02d0a671e8e8fde59544
JayWebDevCom <jaiye.s@gmail.com>
2017-06-12 12:11:44 +0100

    passing transaction type test

--- spec/transaction_spec.js
+++ spec/transaction_spec.js
@@ -8,4 +8,8 @@ describe('transaction', function(){
   it('can be instantiated', function(){
       expect(transaction instanceof Transaction).toBe(true);
   })
+  it('can beof type deposit', function(){
+      var transaction = new Transaction('Deposit')
+      expect(transaction.getType()).toEqual('Deposit');
+  })
 })
