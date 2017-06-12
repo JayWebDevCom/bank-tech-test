da23bc3aca672544d1b02cefe9911ada193c326c
JayWebDevCom <jaiye.s@gmail.com>
2017-06-12 16:38:44 +0100

    red test for printer prints transaction array

--- spec/printerSpec.js
+++ spec/printerSpec.js
@@ -1,11 +1,24 @@
 require('../lib/printer');
-var printer
+var printer;
 
 describe('Printer', function(){
+
   beforeEach(function(){
-    printer = new Printer
+    printer = new Printer()
   })
+
   it('can be instantiated', function(){
     expect(printer instanceof Printer).toBeTruthy();
   })
+
+  it('Has a header constant', function(){
+    expect(printer.HEADER).toEqual('date || credit || debit || balance');
+  })
+
+  it('takes and prints an array', function(){
+    var textToBePrinted = ['apple', 'pear', 'berry', 'grape']
+    var testText = textToBePrinted.join('\n')
+    expect(printer.printStatement(textToBePrinted)).toEqual(testText)
+  })
+
 })
